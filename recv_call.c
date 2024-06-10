05a38b0916ecef109989f0f4b54d3bc421bb663b
Ankit <ankithooda23@gmail.com>
2024-06-10 21:42:33 +0530

    Minor modifications to TCP server and client

--- recv_call.c
+++ recv_call.c
@@ -35,19 +35,24 @@ int main(int argc, char *argv[])
   if (bind(sockfd, (struct sockaddr *) &serv_addr,
            sizeof(serv_addr)) < 0)
     error("ERROR on binding");
+  printf("Waiting for a call\n");
   listen(sockfd,5);
   clilen = sizeof(cli_addr);
   newsockfd = accept(sockfd,
                      (struct sockaddr *) &cli_addr,
                      &clilen);
+  printf("Call received, connection established\n");
   if (newsockfd < 0)
     error("ERROR on accept");
-  bzero(buffer,256);
-  n = read(newsockfd,buffer,255);
-  if (n < 0) error("ERROR reading from socket");
-  printf("Here is the message: %s\n",buffer);
-  n = write(newsockfd,"I got your message",18);
-  if (n < 0) error("ERROR writing to socket");
+
+  while(1) {
+    bzero(buffer,256);
+    n = read(newsockfd,buffer,255);
+    if (n < 0) error("ERROR reading from socket");
+    printf("Here is the message: %s\n",buffer);
+    n = write(newsockfd,"I got your message",18);
+    if (n < 0) error("ERROR writing to socket");
+  }
   close(newsockfd);
   close(sockfd);
   return 0;
